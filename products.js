0b3f88bc51b97719b74f0d7c88e6fa3fbb247305
RahulMG90 <61360857+RahulMG90@users.noreply.github.com>
2022-02-18 15:42:08 +0400

    Add files via upload

---
+++ products.js
@@ -0,0 +1,42 @@
+let products = [
+  {
+    "id": 1001,
+    "title": "Cat Food, 25lb bag",
+    "description": "A 25 pound bag of irresistible, organic goodness for your cat.",
+    "price": 2000.00,
+    "image": "./images/cat.jfif",
+    "availableInventory": 10,
+    "rating": 3
+
+},
+{
+    "id": 1002,
+    "title": "Toaster, 70 kg",
+    "description": "A very beautiful toaster made of metal and marble. It is 70 kg.",
+    "price": 12.00,
+    "image": "./images/toaster.png",
+    "availableInventory": 20,
+    "rating": 4
+
+},
+{
+    "id": 1003,
+    "title": "Dog, 50lb animal",
+    "description": "A 50 pound  animal who is loved by their owners. Your best friend.",
+    "price": 123.00,
+    "image": "./images/dog.jfif",
+    "availableInventory": 2,
+    "rating": 5
+
+},
+{
+    "id": 1004,
+    "title": "A Single Shoe",
+    "description": "A shoe but missing another shoe, good grip but oreo crumbs are the decoration.",
+    "price": 8.00,
+    "image": "./images/shoe.png",
+    "availableInventory": 1,
+    "rating": 2
+
+}
+]
